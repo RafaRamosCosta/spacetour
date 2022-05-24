9c8bdfee01fb2de8865a04f06998a39e0fced788
RafaRamosCosta <rm94281@fiap.com.br>
2022-05-24 16:54:27 -0300

    JavaScript finalizado.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
-function Enviar(){
-   let nome = document.querySelector("#nomeId");
-   let sobrenome = document.querySelector("#sobrenomeId");
-   let genero = document.querySelector("#generoId");
-   let dt_nasc = document.querySelector("#dataId");
-   let texto = document.querySelector("#motivoId");
+function enviar(){
+    let nome = document.querySelector("#nomeId");
+    let sobrenome = document.querySelector("#sobrenomeId");
+    let genero = document.querySelector("#generoId");
+    let dt_nasc = document.querySelector("#dataId");
+    let texto = document.querySelector("#motivoId");
 
    if (nome.value != "" && sobrenome.value != "" && genero.value != "" && dt_nasc.value != "" && texto.value != ""){
        alert("Obrigado " + nome.value + " os seus dados foram encaminhados com sucesso");
    };
 };
+
+document.querySelector('form').addEventListener('submit', enviar);
