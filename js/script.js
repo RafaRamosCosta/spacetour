1f735b76406bf9c8b59a3a1ac085c221135d0568
heitorborbamarini <rm92976@fiap.com.br>
2022-05-25 07:36:07 -0300

    Finalizando formulário e atualizando placeholder

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 function enviar(){
     let nome = document.querySelector("#nomeId");
     let sobrenome = document.querySelector("#sobrenomeId");
+    let email = document.querySelector("#emailid");
+    let cpf = document.querySelector("#cpfId");
     let genero = document.querySelector("#generoId");
     let dt_nasc = document.querySelector("#dataId");
     let texto = document.querySelector("#motivoId");
 
-   if (nome.value != "" && sobrenome.value != "" && genero.value != "" && dt_nasc.value != "" && texto.value != ""){
+   if (nome.value != "" && sobrenome.value != "" && genero.value != "" && dt_nasc.value != "" && texto.value != "" && email.value != "" && cpf.value != ""){
        alert("Obrigado " + nome.value + " os seus dados foram encaminhados com sucesso");
    };
 };
